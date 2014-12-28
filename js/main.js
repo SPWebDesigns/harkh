538b75ef28c8518e85f701216669e96880f3a2ac
SPWebDesigns <santiagoepitluk@gmail.com>
2014-12-27 19:49:57 -0600

    New updates to animations and effects

--- js/main.js
+++ js/main.js
@@ -6,63 +6,52 @@ $(document).ready(function () {
 	 //listener for section change, that shoot a change for sectionHandler
 	 $('.comp-section').on('click',function(){
 	 	sectionsHandler.change($(this).data('section'));
-	 	setSection($(this).data('section'));
+	 	setSection($(this).data('section'));//Set the section for show the correct contact form
 	 });
 
 	 $(".contact-btn").on('click', function(){
 	 	var section= $(this).parent().parent().attr('id');
-	 	console.log(section);
-		switch (section) {
-		    case 'video':
-		        $(".contact-us-video-off").hide();
-			 	$("#main-section").addClass("overlay");
-			 	$("#contact-us-page").show("slow");
-		        break;
-		    case 'web':
-		    	$(".contact-us-web-off").hide();
-			 	$("#main-section").addClass("overlay");
-			 	$("#contact-us-page").show("slow");
-		        console.log(section);
-		        break;
-		    case 'development':
-		    	$(".contact-us-dev-off").hide();
-			 	$("#main-section").addClass("overlay");
-			 	$("#contact-us-page").show("slow");
-		        console.log(section);
-		        break;     
-		    case 'graphic':
-		        $(".contact-us-graph-off").hide();
-			 	$("#main-section").addClass("overlay");
-			 	$("#contact-us-page").show("slow");
-		        console.log(section);
-		        setSection(section);
-		        break;
-		    case 'promotional':
-		        $(".contact-us-obj-off").hide();
-			 	$("#main-section").addClass("overlay");
-			 	$("#contact-us-page").show("slow");
-		        console.log(section);
-		        break;
-		}
-
+	 	$("#main-section").addClass("overlay");
+	 	$("#contact-us-page").css('background-image', 'url(img/contact-us/contact-us-'+section+'.png)');
+		$("#contact-us-page").show("slow");
+		$(".contact-us-"+section+"-off").hide();
+		resetForm();
 	 });
 	 $(".contact-btn-close").on('click', function(){
 	 	var lastSectionSelected=$(this).parent().attr('class');
-	 	console.log(lastSectionSelected);
-	 	console.log("lastSectionSelected");
 		$("#main-section").removeClass("overlay");
 		$("#contact-us-page").hide();
 		$(".contact-us-"+lastSectionSelected+"-off").show();
-	 });/*
-	 $(".overlay").on('click', function(){
-		 	console.log("click overlay");
-			$("#main-section").removeClass("overlay");
-			$("#contact-us-page").hide();
-			$(".contact-us-video-off").show();
-	 });*/
+	 });
+	
 	function setSection(sec){
 		console.log("sec");
 		console.log(sec);
 		$('#wrapper').find('#contact-us-page').removeClass().addClass(sec);
 	}
+	function resetForm(){
+		$("#contact-us-page input, #contact-us-page textarea").val("");
+	}
+
+	/*--- Hover Effects*/
+	 var contact = new TimelineMax();
+	 var btns = new TimelineMax();
+	 $( ".contact-btn" )
+		.mouseover(function() {
+			contact
+				.to(".contact-btn",0.3,{scale:1.2});
+		})
+		.mouseout(function() {
+			contact
+				.to(".contact-btn",0.3,{scale:1});
+	});
+	$( ".btn-developments, .btn-video, .btn-web, .btn-dev, .btn-graph, .btn-obj, .btn-graphicdesign, .btn-promotionalobjects, .submit" )
+		.mouseover(function() {
+			btns
+				.to(this,0.2,{scale:1.1});
+		})
+		.mouseout(function() {
+			btns
+				.to(this,0.2,{scale:1});
+	});
 });
