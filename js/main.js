82f30231d970e72fcadfc698a0c00849a683b3cd
SPWebDesigns <santiagoepitluk@gmail.com>
2014-12-31 12:54:54 -0600

    Updating all

--- js/main.js
+++ js/main.js
@@ -25,13 +25,38 @@ $(document).ready(function () {
 	 });
 	
 	function setSection(sec){
-		console.log("sec");
-		console.log(sec);
 		$('#wrapper').find('#contact-us-page').removeClass().addClass(sec);
+		$('#wrapper').find('#gallery-page').removeClass().addClass(sec);
 	}
 	function resetForm(){
 		$("#contact-us-page input, #contact-us-page textarea").val("");
 	}
+	/*--- Gallery Page ---*/
+	
+	
+	$(".btn-videoproductions, .btn-commercials, .btn-2d3danimations, .btn-augmentedreality, .btn-mobileapps, .btn-videogames, .btn-software, .btn-seopositioning, .btn-webdevelopment, .btn-hosting, .btn-ecommerce, .btn-posters, .btn-illustrations, .btn-logos, .btn-brochures, .btn-personalizedobjects, .btn-merchandising, .btn-corporategifts").on('click', function(){
+	 	var section= $(this).parent().parent().attr('id');
+	 	console.log("section video: "+section);
+	 	$('#'+section).css('opacity','0.3');
+	 	$("#main-section").addClass("overlay");
+	 	$("#gallery-page").css('background-image', 'url(img/contact-us/contact-us-'+section+'.png)');
+	 	var gallery_in = new TimelineMax();
+	 	gallery_in
+	 		.fromTo('#gallery-page', 0.4, {scale:0}, {scale:1},0.2)
+	 		.fromTo('#gallery-page', 0.2, {display: 'none'}, {display: 'block'},0.1);
+		
+	});
+	$(".gallery-btn-close").on('click', function(){
+	 	var lastSectionSelected=$(this).parent().attr('class');
+	 	$('#'+lastSectionSelected).css('opacity','1');
+		$("#main-section").removeClass("overlay");
+		console.log("before gallery none");
+		var gallery_off = new TimelineMax();
+		gallery_off
+	 		.fromTo('#gallery-page', 0.4, {scale:1}, {scale:0},0.2)
+	 		.fromTo('#gallery-page', 0.2, {display: 'block'}, {display: 'none'},0.1);
+	 	console.log("after gallery none");
+	 });
 
 	/*--- Hover Effects*/
 	 var contact = new TimelineMax();
@@ -45,8 +70,9 @@ $(document).ready(function () {
 			contact
 				.to(".contact-btn",0.3,{scale:1});
 	});
-	$( ".btn-developments, .btn-video, .btn-web, .btn-dev, .btn-graph, .btn-obj, .btn-graphicdesign, .btn-promotionalobjects, .submit" )
+	$( ".btn-developments, .btn-video, .btn-web, .btn-dev, .btn-graph, .btn-obj, .btn-graphicdesign, .btn-promotionalobjects, .btn-videoproductions, .btn-commercials, .btn-2d3danimations, .btn-augmentedreality, .btn-mobileapps, .btn-videogames, .btn-software, .btn-seopositioning, .btn-webdevelopment, .btn-hosting, .btn-ecommerce, .btn-posters, .btn-illustrations, .btn-logos, .btn-brochures, .btn-personalizedobjects, .btn-merchandising, .btn-corporategifts,.submit " )
 		.mouseover(function() {
+			$(this).css('cursor','pointer');
 			btns
 				.to(this,0.2,{scale:1.1});
 		})
